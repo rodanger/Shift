dbcf250c94e79f7d68d1b9dd7871ece9507e5bf2
rodanger <71657333+rodanger@users.noreply.github.com>
2026-03-17 13:36:42 -0400

    Change Django settings module to 'shift.settings'

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shift.settings')
     
     # Add the project root to sys.path so 'apps' is importable
     sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
